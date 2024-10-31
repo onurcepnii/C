511322b162459265a49bf240489dd0a1ea595fa9
Onur Çepni <94144681+onurcepnii@users.noreply.github.com>
2024-10-31 09:47:04 +0300

    Create endianness_check.c

---
+++ endianness_check.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    
+    int x  = 1;
+    
+    if(*(char*)&x)
+        printf("Big Endian");
+        
+    else
+        printf("Little Endian");
+
+    return 0;
+}
